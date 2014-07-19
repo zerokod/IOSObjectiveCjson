1251f0074f0072bcaff1582ca295bb43c6389e0c
zerokod <zeroblog27@gmail.com>
2014-07-20 00:41:53 +0200

    Initial Commit

---
+++ AppCode/ZRK_AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZRK_AppDelegate.h
+//  AppCode
+//
+//  Created by Roberta on 20/07/14.
+//  Copyright (c) 2014 Zerokod Interactive Media Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZRK_AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AppCode/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AppCode
+//
+//  Created by Roberta on 20/07/14.
+//  Copyright (c) 2014 Zerokod Interactive Media Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZRK_AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZRK_AppDelegate class]));
+    }
+}
